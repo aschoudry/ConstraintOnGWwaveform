d8523fcf1729fca45766fcb94d3533cf888a6c56
ashok <ashok91choudhary@gmail.com>
2020-05-04 15:17:55 -0400

    updated notes for psi2 for BOB

---
+++ script/generate_EOB_wavaform.py
@@ -0,0 +1,33 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from pycbc.waveform import get_td_waveform
+
+## Ignore the PN Orders, the main function to get the waveform is get_td_waveform.
+
+pnorder = ['LAL_PNORDER_ONE', 'LAL_PNORDER_TWO','LAL_PNORDER_THREE','LAL_PNORDER_NUM_ORDER']
+HP , HC = get_td_waveform(approximant = "SEOBNRv4",mass1 = 20 , mass2 = 20 , delta_t = 1.0/4096 , f_lower = 20,LALPNOrder = pnorder[-1])
+
+
+H_tot = HP + 1j* HC
+
+H_phase=np.unwrap(np.angle(H_tot))
+H_amp = abs(H_tot)/max(abs(H_tot))
+H_complex = np.array(H_amp*np.exp(1j*H_phase))
+H_time = np.linspace(HP.sample_times[0],HP.sample_times[-1], len(H_amp))
+
+
+print(max(H_complex.real))
+plt.plot(HP.sample_times/(40*4.9*pow(10, -6)), HP/max(abs(H_tot)))
+plt.plot(HP.sample_times/(40*4.9*pow(10, -6)), H_complex.real , '--k')
+
+#plt.xlim(-200, 50)
+plt.show()
+
+
+f = open("/home/ashok/constraintongwwaveform/data/EOB_wf.txt","w") 
+
+for i in range(len(HP)):
+    f.write("%E %E %E\n" % (HP.sample_times[i]/(40*4.9*pow(10, -6)), H_amp[i], H_phase[i])) 
+
+f.close() 
