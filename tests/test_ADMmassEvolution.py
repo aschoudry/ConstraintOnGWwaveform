98ef7d2d4dd220a849d10442ace1ff38083285a7
Ashok Choudhary <aschoudhary@mix.wvu.edu>
2020-07-13 14:18:22 -0400

    udated notes and plots

--- tests/test_ADMmassEvolution.py
+++ tests/test_ADMmassEvolution.py
@@ -8,15 +8,18 @@ import PN_radiationReaction as PN_RR
 
 def CircularPr(r, nu):
     return np.sqrt(ODEs.Ap(r, nu)*(r**2)/(2*ODEs.A(r, nu)/r - ODEs.Ap(r, nu)))
+
+def Psi2p(r, nu, M):
+    return -nu*M*(1.0 - 2*M/r)/np.sqrt(1-3*M/r)
  
 ## Initial conditions ##
-r0 = 7
-p=0.25 # parameter nu
-
+r0 = 15.0
+p=0.249 # parameter nu
+M=1
 w0 = r0, 0.0, 0, CircularPr(r0, p) 
 
 #t_vec = np.arange(0, 525, 0.5)
-t_vec = np.arange(0, 7000, 1)
+t_vec = np.arange(0, 5900, 1)
 
 
 ## Solve ODEs
@@ -47,16 +50,53 @@ plt.show()
 plt.plot(t_vec-t_vec[-1], H_EOB-H_EOB[0])
 plt.show()
 
+#plt.figure()
+#plt.plot(t_vec-t_vec[-1], H_EOB-H_EOB[0], label=r'$EOB \, \psi2$ = ')
+#plt.xlabel(r'$time$')
+#plt.ylabel(r'$\psi_{2}$')
+#plt.xlim(-50, 0)
+#plt.ylim(-0.065, -0.020)
+#plt.legend()
+#plt.savefig('/home/aschoudhary/constraintongwwaveform/plots/Psi2EOB.pdf')
+#plt.show()
+
 plt.figure()
-plt.plot(t_vec-t_vec[-1], H_EOB-H_EOB[0], label=r'$EOB \, \psi2$ = ')
+h2 = ODEs.Rh22(r, p_r, phi, p_phi, p)
+plt.ylim(-1.0, 1.0)
 plt.xlabel(r'$time$')
-plt.ylabel(r'$\psi_{2}$')
-plt.xlim(-50, 0)
-plt.ylim(-0.065, -0.020)
+plt.ylabel(r'$h_{22}$')
+plt.plot(t_vec-t_vec[-1], h2.real)
 plt.legend()
-plt.savefig('/home/aschoudhary/constraintongwwaveform/plots/Psi2EOB.pdf')
+plt.savefig('/home/aschoudhary/constraintongwwaveform/plots/h22EOB.pdf')
 plt.show()
 
-h2 = ODEs.Rh22(r, p_r, phi, p_phi, p)
-plt.plot(t_vec, h2)
+plt.figure()
+psi2p = Psi2p(r,p,M )
+#plt.ylim(-1.0, 1.0)
+plt.xlabel(r'$time$')
+plt.ylabel(r'$\psi_{2}^{0(1)}$')
+plt.plot(t_vec-t_vec[-1], psi2p)
+plt.legend()
+#plt.savefig('/home/aschoudhary/constraintongwwaveform/plots/OrbitEOB'+str(r0)+'.pdf')
+plt.show()
+
+H2 = abs(ODEs.Rh22(r, p_r, phi, p_phi, p))
+H2p2 = abs(ODEs.Rh22(r, p_r, phi, p_phi, p))**2
+H2p2OM = abs(ODEs.Rh22(r, p_r, phi, p_phi, p))**2 *ODEs.dphi_by_dt(r, p_r, phi, p_phi, p)
+
+H2 = H2-H2[0]
+H2p2 = H2p2-H2p2[0]
+H2p2OM = H2p2OM-H2p2OM[0]
+
+print(H2[1:10])
+print(H2p2[1:10])
+print(H2p2OM[1:10])
+
+heff = abs(ODEs.Heff(r, p_r, phi, p_phi, p))
+heff = heff - heff[0]
+
+print(heff[1:10])
+
+plt.plot(t_vec, -H2p2OM)
+plt.plot(t_vec, heff)
 plt.show()
---
+++ tests/test_Ps2EOb.py
@@ -0,0 +1,59 @@
+import sys
+sys.path.insert(0, '/home/aschoudhary/constraintongwwaveform/src')
+
+import numpy as np
+import HamiltonianCoupledODE as ODEs
+import matplotlib.pyplot as plt
+import PN_radiationReaction as PN_RR
+
+def CircularPr(r, nu):
+    return np.sqrt(ODEs.Ap(r, nu)*(r**2)/(2*ODEs.A(r, nu)/r - ODEs.Ap(r, nu)))
+
+def Psi2p(r, nu, M):
+    return -nu*M*(1.0 - 2*M/r)/np.sqrt(1-3*M/r)
+
+def Psi2_0_Growth(r, p_r, phi, p_phi, p):
+    HEOB = ODEs.Heob(r, p_r, phi, p_phi, p)
+    HEOB = HEOB-HEOB[0]
+    return  HEOB 
+
+def Psi2_fromWaveform(t_vec, r, p_r, phi, p_phi, p):
+    dt = t_vec[1]-t_vec[0]
+    sigma_sigma_Dot =  (abs(ODEs.Rh22(r, p_r, phi, p_phi, p))**2)*ODEs.dphi_by_dt(r, p_r, phi, p_phi, p)
+    abs_Sigma_Dot_sqr = (ODEs.dphi_by_dt(r, p_r, phi, p_phi, p)*abs(ODEs.Rh22(r, p_r, phi, p_phi, p)))**2
+    Int_abs_Sigma_Dot_sqr = np.cumsum(abs_Sigma_Dot_sqr)*dt
+    Psi2_diff = sigma_sigma_Dot - Int_abs_Sigma_Dot_sqr -  abs(ODEs.Rh22(r, p_r, phi, p_phi, p))
+    Psi2_diff = Psi2_diff - Psi2_diff[0]
+    return Psi2_diff
+
+ 
+## Initial conditions ##
+r0 = 15.0
+p=0.25 # parameter nu
+M=1
+w0 = r0, 0.0, 0, CircularPr(r0, p) 
+
+#t_vec = np.arange(0, 525, 0.5)
+t_vec = np.arange(0, 5900, 1)
+
+
+## Solve ODEs
+yvec = ODEs.Coupled_HamiltonianODEs_solver(w0, t_vec, p)
+
+r, p_r, phi, p_phi = yvec[:,0], yvec[:,1], yvec[:,2], yvec[:,3]
+
+Omega = ODEs.dphi_by_dt(r, p_r, phi, p_phi, p)
+
+H_EOB = ODEs.Heob(r, p_r, phi, p_phi, p)
+
+plt.plot(t_vec, Psi2_0_Growth(r, p_r, phi, p_phi, p), label = 'EOB')
+plt.plot(t_vec, Psi2_fromWaveform(t_vec, r, p_r, phi, p_phi, p))
+plt.xlim(0, 5900)
+plt.ylim(-0.3, 0.1)
+plt.xlabel(r'$time$')
+plt.ylabel(r'$\psi_{2}^{0}$')
+plt.legend()
+plt.savefig('/home/aschoudhary/constraintongwwaveform/plots/Psi2EOBHamiltonia.pdf')
+plt.show()
+
+print(ODEs.Rh22(r, p_r, phi, p_phi, p).real)
