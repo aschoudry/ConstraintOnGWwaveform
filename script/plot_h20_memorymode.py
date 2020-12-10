7a26b4d63a61e911a6568ec03f0751dc05ee0ebd
Ashok <aschoudhary@mix.wvu.edu>
2020-12-10 00:25:22 -0500

    comper h20 memory from SXS and Favata paper

---
+++ script/plot_h20_memorymode.py
@@ -0,0 +1,69 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sxs
+
+
+# Load SXS data
+catalog = sxs.load("catalog")
+file_num_S0 = '0001'
+file_num_S0p99 = '0178'
+
+L1=5
+
+extrapolation_order = 4
+w_L_S0 = sxs.load("SXS:BBH:"+str(file_num_S0)+"/Lev("+str(L1)+")/rhOverM", extrapolation_order=extrapolation_order)
+w_L_S0p99 = sxs.load("SXS:BBH:"+str(file_num_S0p99)+"/Lev("+str(L1)+")/rhOverM", extrapolation_order=extrapolation_order)
+
+
+metadata_S0 = sxs.load("SXS:BBH:"+str(file_num_S0)+"/Lev("+str(L1)+")/metadata.json")
+metadata_S0p99 = sxs.load("SXS:BBH:"+str(file_num_S0p99)+"/Lev("+str(L1)+")/metadata.json")
+
+#Load data used in memory paper
+#data location
+file_location_S0 ='/home/ashok/gravitational_wave_memory_project/data/NonSpinning_differentMassRatio/Memory_data/'
+file_location_S0p99 ='/home/ashok/gravitational_wave_memory_project/data/SXSdata/Spinning_binary_with_SpinAligned_27Dec/Memory_data/'
+
+#import data
+filename='q1'
+datafile_hMemNR_S0='rMPsi4_noSpin_'+filename+'dataClean_hMemNR.dat'
+timeNR_S0, hmem_S0, h_mem_plus_S0 = np.loadtxt(file_location_S0+datafile_hMemNR_S0, unpack=True)
+hmem_S0*=17.0
+
+datafile_hMemNR_S0p99='rMPsi4_Sz1_0p99_Sz2_0p99_q1p5dataN4Clean_hMemNR.dat'
+timeNR_S0p99, hmem_S0p99, h_mem_plus_S0p99 = np.loadtxt(file_location_S0p99+datafile_hMemNR_S0p99, unpack=True)
+hmem_S0p99*=17.0
+
+
+# Spinweighted spherical harmonics
+m2Y20 = 3.0/4.0*np.sqrt(5.0/6.0/np.pi)
+
+# Compute memory terms
+
+reference_index_S0 = w_L_S0.index_closest_to(metadata_S0.reference_time)
+reference_index_S0p99 = w_L_S0p99.index_closest_to(metadata_S0p99.reference_time)
+
+h_sliced_S0 = w_L_S0[reference_index_S0:]
+h_sliced_S0p99 = w_L_S0p99[reference_index_S0p99:]
+
+
+h_mem_S0 = sxs.waveforms.memory.J_E(h_sliced_S0, start_time=1000)
+h_mem_S0p99 = sxs.waveforms.memory.J_E(h_sliced_S0p99, start_time=1000)
+
+time_S0 = h_mem_S0.t 
+time_S0p99 = h_mem_S0p99.t 
+
+
+h_mem_20_S0 = (h_mem_S0)[:,h_mem_S0.index(2, 0)].data.real*m2Y20
+h_mem_20_S0p99 = (h_mem_S0p99)[:,h_mem_S0p99.index(2, 0)].data.real*m2Y20
+
+plt.plot(time_S0p99-time_S0p99[0], h_mem_20_S0p99,'c', label=r"0.99 SXS code")
+plt.plot(time_S0-time_S0[0], h_mem_20_S0,'k', label=r"0.00 SXS code")
+plt.plot(timeNR_S0-timeNR_S0[0], hmem_S0,'r--', label=r"0.00 Favata's epression")
+plt.plot(timeNR_S0p99-timeNR_S0p99[0], hmem_S0p99,'b--', label=r"0.00 Favata's epression")
+
+plt.xlabel(r"$J_E^{(2,0)}$")
+plt.xlabel(r"$(t_{\mathrm{corr}} - r_\ast)/M$")
+plt.legend()
+
+plt.show()
+
