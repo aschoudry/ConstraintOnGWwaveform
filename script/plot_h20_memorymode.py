c41d41c9f08d1bd1a93e41d267951c113a06dc32
Ashok <aschoudhary@mix.wvu.edu>
2020-12-10 18:16:10 -0500

    updated code

--- script/plot_h20_memorymode.py
+++ script/plot_h20_memorymode.py
@@ -21,6 +21,7 @@ w_L_S0p99 = sxs.load("SXS:BBH:"+str(file_num_S0p99)+"/Lev("+str(L1)+")/rhOverM",
 metadata_S0 = sxs.load("SXS:BBH:"+str(file_num_S0)+"/Lev("+str(L1)+")/metadata.json")
 metadata_S0p99 = sxs.load("SXS:BBH:"+str(file_num_S0p99)+"/Lev("+str(L1)+")/metadata.json")
 
+'''
 #Load data used in memory paper
 #data location
 file_location_S0 ='/home/ashok/gravitational_wave_memory_project/data/NonSpinning_differentMassRatio/Memory_data/'
@@ -82,4 +83,63 @@ plt.xlabel(r"$(t_{\mathrm{corr}} - r_\ast)/M$")
 plt.legend()
 plt.savefig("/home/ashok/constraintongwwaveform/plots/MemoryComparisioSXSpkgVsFavata.pdf")
 plt.show()
+'''
+#Compute Psi2
+ADM_Mass_tot = metadata_S0['initial_mass1'] + metadata_S0['initial_mass2']
+print(ADM_Mass_tot)
+
+h = w_L_S0
+reference_index =w_L_S0.index_closest_to(metadata_S0.reference_time)
+h_sliced = w_L_S0[reference_index:]
+h_mem = sxs.waveforms.memory.J_E(h_sliced, start_time=1000)
+
+h_mem_20 = (h_mem)[:,h_mem.index(2, 0)].data.real
+h_mem_21 = (h_mem)[:,h_mem.index(2, 1)].data.real
+h_mem_22 = (h_mem)[:,h_mem.index(2, 2)].data.real
+
+
+mts_h = sxs.waveforms.memory.MTS(h_sliced)
+eth2_h = mts_h.eth.eth
+h_dot_h_bar = (mts_h.dot*mts_h.bar)
+
+psi2 = 0.25*(eth2_h+h_dot_h_bar).re 
+psi2_JE22 = psi2# + 0.25*h_mem
+
+time = psi2.t 
+
+psi2_20 = (psi2)[:,psi2.index(2, 0)].data.real
+psi2_21 = (psi2)[:,psi2.index(2, 1)].data.real
+psi2_22 = (psi2)[:,psi2.index(2, 2)].data.real
+
+
+#Add memory terms to the equations
+psi2_JE_20 = psi2_20 + h_mem_20
+psi2_JE_21 = psi2_21 + h_mem_21
+psi2_JE_22 = psi2_22 + h_mem_22
+
+
+plt.plot(time, psi2_20, label=r"$\Psi^{(2,0)}$")
+plt.plot(time, psi2_21, label=r"$\Psi^{(2,1)}$")
+plt.plot(time, psi2_22, label=r"$\Psi^{(2,2)}$")
+plt.ylabel(r"$\Psi^{(l,m)}$")
+plt.xlabel(r"$(t_{\mathrm{corr}} - r_\ast)/M$")
+plt.xlim(9400, 9600)
+plt.title(r'$\Re[\Psi] = \frac{1}{4}\{-\Re[\eth^2 h]-\Re[\dot{h}\bar{h}]\}$')
+plt.legend()
+plt.savefig("/home/ashok/constraintongwwaveform/plots/Psi2_SXSequation_Nomem.pdf")
+
+plt.show()
+
+plt.plot(time, psi2_JE_20, label=r"$\Psi^{(2,0)}$")
+plt.plot(time, psi2_JE_21, label=r"$\Psi^{(2,1)}$")
+plt.plot(time, psi2_JE_22, label=r"$\Psi^{(2,2)}$")
+plt.ylabel(r"$\Psi^{(l,m)}$")
+plt.xlabel(r"$(t_{\mathrm{corr}} - r_\ast)/M$")
+plt.xlim(9400, 9600)
+plt.title(r'$\Re[\Psi] = \frac{1}{4}\{\int^{u}_{0}\|\dot{h}\|^2 du - \Re[\eth^2 h]-\Re[\dot{h}\bar{h}]\}$')
+plt.legend()
+plt.savefig("/home/ashok/constraintongwwaveform/plots/Psi2_SXSequation_mem.pdf")
+
+plt.show()
+
 
